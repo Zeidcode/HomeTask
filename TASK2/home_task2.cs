6264cb0a040c75c8a130c8e51a83ed044a703d99
ZEID <zeidcode@gmail.com>
2022-08-07 16:02:47 +0400

    Макс_из_трех

---
+++ TASK2/home_task2.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите ваши чиcла: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("Nubmer Max = " + num1);
+}
+else if (num2 > num3)
+{
+    Console.WriteLine("Number Max = " + num2);
+}
+else
+{
+    Console.WriteLine("Number Max = " + num3);
+}
