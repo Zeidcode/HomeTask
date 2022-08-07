ccd68c10342ba76349afb92ae93a113cc730e95e
ZEID <zeidcode@gmail.com>
2022-08-07 14:52:36 +0400

    Макс_из_двух

---
+++ TASK1/home_task1.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите ваши чиисла:");
+
+int numA = Convert.ToInt32(Console.ReadLine());  
+int numB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(numA>numB)
+{
+    max = numA;
+    Console.WriteLine(max);
+}
+else
+{
+    Console.WriteLine(numB);
+}
