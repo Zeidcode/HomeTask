535da362395d1834fbb203f268297166b7b6a024
ZEID <zeidcode@gmail.com>
2022-08-07 16:56:42 +0400

    проверка_на_четность

---
+++ TASK3/home_task3.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите ваше число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0) 
+    Console.WriteLine("Четное");
+else
+    Console.WriteLine("Нечетное");
